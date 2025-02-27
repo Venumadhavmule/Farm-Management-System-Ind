21284d18ad365e329fdb131d17ed77c86dfcc291
Venumadhava Reddy Mule <venumadhavamule@gmail.com>
2025-02-27 20:07:39 +0530

    modified some conflicts

--- config/database.js
+++ config/database.js
@@ -10,17 +10,5 @@ const connectDB = mongoose.connect(Uri,{
   console.error('MongoDB connection error:', error);
   process.exit(1);
 })
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(Uri, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('MongoDB connected');
-//   } catch (error) {
-//     console.error('MongoDB connection error:', error);
-//     process.exit(1);
-//   }
-// };
 
 module.exports = connectDB;
